803a3f2bfbb050ed4481fb84662aa7a122cbc175
Ahmed Alnasser <a3alnasser@gmail.com>
2023-11-18 21:01:26 +0300

    CreateTable function to display any future tables

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 import json
 import os
+
 # Brings the JSON file and convert it to a dictionary
 with open("patients.txt", 'r') as file: 
     patients = json.loads(file.read())
@@ -7,11 +8,13 @@ with open("patients.txt", 'r') as file:
 #for more simplicity so we can recall it later any time
 def rewrite():
     with open("patients.txt", 'w') as file:
-            file.write(json.dumps(patients, indent=2)) 
+            file.write(json.dumps(patients, indent=2, sort_keys=True)) 
 
 def AddPatient():
     ID = input("Please enter the patient ID: ")
-    if ID in patients:
+    if ID not in ID.isdigit():
+        print("ID numbers can only consist of digits") # To ensure the user can't enter letters, this is helpful for sorting later
+    elif ID in patients:
         print("Patient ID already exist")
     else:
         name = input("Please enter the patient's name: ")
@@ -92,4 +95,35 @@ def add_medical_history():
 
     return medical_history_data
 
-add_medical_history()
+def createTable(header, givenValues):
+    # givenValues must be nested lists where each list is a row
+    structure = [header]
+    structure.extend(givenValues)
+    columns_width = []
+    for row, row_values in enumerate(structure):
+        for column, element in enumerate(row_values):
+            if row == 0:
+                columns_width.append(len(str(element)))
+                continue
+            if len(str(element)) > columns_width[column]:
+                columns_width[column] = len(str(element))
+    rows = header[:]
+    rows.append(givenValues)
+    horizontalDividerLen = (len(columns_width)-1)*3 # Adds the sum of widths of all vertical dividers
+    for i in columns_width:
+        horizontalDividerLen+=i
+    
+    print("")
+    for currentRow in structure:
+        for cellNumber, cellValue in enumerate(currentRow):
+            print(cellValue + " "*(columns_width[cellNumber]-len(cellValue)), end='')
+            if (cellNumber+1) != len(currentRow):
+                print(" | ", end='')
+            else:
+                print("") # to go to next line
+        if (structure.index(currentRow)+1) != len(structure):
+            print("-"*horizontalDividerLen)
+    print("")
+
+# A useful example of how this function works in case tests are needed
+# createTable(list(choice(list(patients.values())).keys()), [["@"*randint(1,4) for i in range(7)] for _ in range (3)])
