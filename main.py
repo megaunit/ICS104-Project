d672838693555e846ea1e357eb9bc74418b0c5cb
Ahmed Alnasser <a3alnasser@gmail.com>
2023-11-27 21:23:24 +0300

    fixing multiple functions

--- main.py
+++ main.py
@@ -11,21 +11,21 @@ def rewrite():
             file.write(json.dumps(patients, indent=2)) 
 
 def AddPatient():
-    ID = input("Please enter the patient ID: ")
+    ID = input("Enter the patient ID: ")
     if ID not in ID.isdigit():
         print("ID numbers can only consist of digits") # To ensure the user can't enter letters, this is helpful for sorting later
     elif ID in patients:
         print("Patient ID already exist")
     else:
-        name = input("Please enter the patient's name: ")
-        gender = input("Please enter the patient's gender: ")
-        year = input("Please enter the patient's year of birth: ") ## make limitation for the current year at max
-        month = input("Please enter the patient's month of birth: ") ## limit it between 1 and 12
-        day = input("Please enter the patient's day of birth: ") ## limit it in 1 and 30
-        blood = input("Please enter the patient's blood type: ")
-        city = input("Please enter the patient's city: ")
-        number = input("Please enter the patient's number: ") ## consider not repeating the number
-        email = input("Please enter the patient's email address: ") ## consider not repeating the email
+        name = input("Enter the patient's name: ")
+        gender = input(f"Enter the {name}'s gender: ")
+        year = input(f"Enter the {name}'s year of birth: ") ## make limitation for the current year at max
+        month = input(f"Enter the {name}'s month of birth: ") ## limit it between 1 and 12
+        day = input(f"Enter the {name}'s day of birth: ") ## limit it in 1 and 30
+        blood = input(f"Enter the {name}'s blood type: ")
+        city = input(f"Enter the {name}'s city: ")
+        number = input(f"Enter the {name}'s number: ") ## consider not repeating the number
+        email = input(f"Enter the {name}'s email address: ") ## consider not repeating the email
         
         birthday = f"{day}/{month}/{year}"
         patients[ID] = {
@@ -53,51 +53,47 @@ def UpdateInfo():
         else:
             print("Choose only from the given choices")
     if info.lower() == "birthday":
-        year = input("Please enter the new year of birth: ") ## mention the patient name instead of just saying "the new info"
-        month = input("Please enter the new month of birth: ") 
-        day = input("Please enter the new day of birth: ")
+        year = input("Enter the new year of birth: ") ## mention the patient name instead of just saying "the new info"
+        month = input("Enter the new month of birth: ") 
+        day = input("Enter the new day of birth: ")
         new_info = f"{day}/{month}/{year}"
     else: ## more elifs (formatting email and phone number. Do that also above in the AddPatient)
-        new_info = input(f"Please enter the new {info}: ")
+        new_info = input(f"Enter the new {info}: ")
     patients[ID][info.lower()] = new_info
 
     rewrite()
 
-def add_medical_history():
-    ID = input("Please enter the patient ID: ")
-    patient_data = patients.get(ID)
-    if ID not in patients:
+def AddMedicalHistory():
+    ID = input("Enter the patient ID: ")
+    patient_data = patients.get(ID) # .get() will return None if ID doesn't exist
+    if patient_data == None:
         print("ID does not exist")
         return 
-
     patient_name = patient_data.get("name")
-    if ID not in patients:
-        print("Patient name is not registerd.")
-        return 
-
-    doctor = input(f"Please enter {patient_name}'s doctor: ")
-    last_visit=input(f"Please enter {patient_name}'s last visit: ")
-    diagnosis = input(f"Please enter {patient_name}'s diagnosis: ")
-    medications = input(f"Please enter {patient_name}'s medications: ")
-    advice = input(f"Please enter {patient_name}'s advice: ")
+    
+    doctor = input(f"Enter {patient_name}'s doctor: ")
+    last_visit_year=input(f"Enter {patient_name}'s year of last visit: ")
+    last_visit_month=input(f"Enter {patient_name}'s month of last visit: ")
+    last_visit_day=input(f"Enter {patient_name}'s day of last visit: ")
+    diagnosis = input(f"PEter {patient_name}'s diagnosis: ")
+    medicine = input(f"Enter {patient_name}'s medicine: ")
+    advice = input(f"Enter {patient_name}'s advice: ")
+    last_visit = f"{last_visit_day}/{last_visit_month}/{last_visit_year}"
 
     medical_history_data = {
-        "name": patient_name,
         "Correspondent Doctor": doctor,
         "Last Visit": last_visit,
         "diagnosis": diagnosis,
-        "medications": medications,
+        "medications": medicine,
         "advice": advice
     }
-    directory="C:\\Users\\GAMER\\Desktop\\ICS\\test\\ICS104-Project\\Patient" #change the path to your patient folder 
-    file_name=ID + ".txt"
-    full_path=os.path.join(directory,file_name)
-    with open(full_path, 'a') as file2:
-        file2.write(json.dumps({ID: medical_history_data}) + '\n')
+
+    patients[ID]["medical_history"] = medical_history_data
+    rewrite()
 
     return medical_history_data
 
-def displayTable(header, givenValues):
+def DisplayTable(header, givenValues):
     # givenValues must be nested lists where each list is a row
     structure = [header]
     structure.extend(givenValues)
@@ -128,31 +124,31 @@ def displayTable(header, givenValues):
 # A useful example of how this function works in case tests are needed
 # createTable(list(choice(list(patients.values())).keys()), [["@"*randint(1,4) for i in range(7)] for _ in range (3)])
 
+def DisplayData():
+    header = ['ID', 'Name', 'Gender', 'Birthday', 'Blood', 'City', 'Number', 'Email']
+    data = [] # list which will contain all patients data as nested lists
 
-def sortData(sorting):
-    # sorts data based on user choice
-    data = []
-    index = 0
+    # adds patients data to the data list
     for patient in patients:
-        data.append([])
-        for info in patients[patient]:
-            # print(info)
-            # data[index].append(patients[patients][info])
-            # print(patients[patient][info])
-            data[index].append(patients[patient][info])
-        # print("\n\n\n")
-        index += 1
+        data.append([patients[patient][info] for info in patients[patient] if info != 'medical_history'])
+
+    # ask the user how he wants to sort the data
+    while True:
+        sorting = input("(ID, Name, City)\nChoose how do you want to sort the data in the table: ")
+        sorting = sorting.lower()
+        if sorting not in ['name', 'id', 'city']:
+            print("You can only choose from the three given sorting methods")
+        else:
+            break
     
+    # sorts data list according to the user's choice
     if sorting == "id":
         data.sort() # sorts by first element, which is ID in this  case
     elif sorting == "name":
-        data.sort(key= lambda x:x[1])
+        data.sort(key= lambda x:x[1]) # lambda function helps specifying index of element which will be key of sorting
     elif sorting == "city":
-        data.sort(key= lambda x:x[5])
-    return data
+        data.sort(key= lambda x:x[5]) 
 
-def DisplayData():
-    header = ['ID', 'Name', 'Gender', 'Birthday', 'Blood', 'City', 'Number', 'Email']
-    sort = input("(ID, Name, City)\nChoose how do you want to sort the data in the table: ")
-    sort = sort.lower()
-    displayTable(header, sortData(sort))
+    DisplayTable(header, data)
+
+DisplayData()
